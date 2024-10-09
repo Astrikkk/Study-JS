ec0678dae212d215cea3dbd8c63ae7721dbf1a91
Astragor <101702563+Astrikkk@users.noreply.github.com>
2024-10-09 11:16:42 +0200

    0.1

---
+++ testscript.js
@@ -0,0 +1,28 @@
+
+
+// let MyName
+// MyName = "Astragor"
+
+// console.log(MyName)
+
+// let objectA = {
+//     name: "nigga",
+//     age: 18
+// }
+
+// let objectB = objectA;
+
+// objectB.age-=20;
+
+// console.log(objectA.age);
+
+let MyArray = [4,56,3,12,645,23,12]
+
+MyArray.forEach(el => console.log(el))
+
+for(let i = 0; i < 10; i++){
+    for(let j = 0; j < 10; j++){
+        if(i<=j)
+            console.log("#")
+    }
+}
